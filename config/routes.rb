11407624cc422584cab54c223164e651593eb393
Zack Brown <zack.t.brown@gmail.com>
2018-07-27 07:16:46 -0400

    fuzzyish search on cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :decks, only: [:index, :show] 
   resources :card_recommendations, only: [:index]
-  resources :cards, only: [:show]
+  resources :cards, only: [:show, :index]
   root 'home#index' 
 end
