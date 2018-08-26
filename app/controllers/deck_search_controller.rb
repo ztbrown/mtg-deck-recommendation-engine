31912b0df101c8e8d56a1364abdc0838bf07c0f8
Zack Brown <zack.t.brown@gmail.com>
2018-08-26 12:32:17 -0400

    strike out cards not in decks

--- app/controllers/deck_search_controller.rb
+++ app/controllers/deck_search_controller.rb
@@ -1,5 +1,5 @@
 class DeckSearchController < ApplicationController
   def index
-    render json: Neo4j::ActiveBase.current_session.query('WITH {ids} as cards MATCH (c:Card)<-[:main_deck]-(d:Deck) WHERE c.id in cards WITH d, size(cards) as inputCnt, count(DISTINCT c) as cnt RETURN d, 100.0 * (cnt * 1.0/inputCnt) as Strength ORDER BY Strength DESC', ids: params[:cards])
+    render json: Neo4j::ActiveBase.current_session.query('WITH {ids} as cards MATCH (c:Card)<-[:main_deck]-(d:Deck) WHERE c.id in cards WITH d, collect(c.name) as deck_cards, size(cards) as inputCnt, count(DISTINCT c) as cnt RETURN d, deck_cards, 100.0 * (cnt * 1.0/inputCnt) as Strength ORDER BY Strength DESC', ids: params[:cards])
   end
 end
